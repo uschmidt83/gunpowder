4cbabda6030881e48a80fec7169b2d75f19173f2
rceballos98 <rceballos98@gmail.com>
2017-11-01 23:05:00 +0100

    Progress on predict synapses node
    Stopped because of GPU crash/stalling

--- gunpowder/nodes/predict_synaptic_pairs.py
+++ gunpowder/nodes/predict_synaptic_pairs.py
@@ -5,6 +5,8 @@ import pdb
 
 from gunpowder.volume import Volume, VolumeTypes
 from gunpowder.points_spec import PointsSpec
+from gunpowder.points import *
+
 
 from .batch_filter import BatchFilter
 from scipy import ndimage
@@ -15,13 +17,13 @@ logger = logging.getLogger(__name__)
 
 class PredictSynapticPairs(BatchFilter):
 
-    def __init__(self, affinity_pred_volume, segmentation_volume, pred_pre_syn_points,
-     pred_post_syn_points, affinity_vectors, threshold = 500):
+    def __init__(self, affinity_pred_volume, segmentation_volume, pred_presyn_points_name,
+     pred_postsyn_points_name, affinity_vectors, threshold = 500):
 
         self.affinity_pred_volume = affinity_pred_volume
         self.segmentation_volume = segmentation_volume
-        self.pred_pre_syn_points = pred_pre_syn_points
-        self.pred_post_syn_points = pred_post_syn_points
+        self.pred_presyn_points_name = pred_presyn_points_name
+        self.pred_postsyn_points_name = pred_postsyn_points_name
         self.affinity_vectors = affinity_vectors
         self.threshold = threshold
         self.skip_next = False
@@ -51,21 +53,21 @@ class PredictSynapticPairs(BatchFilter):
         spec = PointsSpec()
         spec.roi = segmentation_spec.roi
 
-        self.provides(self.pred_pre_syn_points, spec)
-        self.provides(self.pred_post_syn_points, spec)
+        self.provides(self.pred_presyn_points_name, spec)
+        self.provides(self.pred_postsyn_points_name, spec)
 
     def prepare(self, request):
 
         # do nothing if no gt affinities were requested
-        # if not (self.pred_pre_syn_points in request and self.pred_post_syn_points in request):
+        # if not (self.pred_presyn_points_name in request and self.pred_postsyn_points_name in request):
         #     logger.warn("no affinites requested, will do nothing")
         #     self.skip_next = True
         #     return
         logger.debug("predict prepare start" )
 
 
-        points_request_roi = (request[self.pred_pre_syn_points].roi).union(
-            request[self.pred_post_syn_points].roi)
+        points_request_roi = (request[self.pred_presyn_points_name].roi).union(
+            request[self.pred_postsyn_points_name].roi)
 
         logger.debug("downstream points request: " + str(points_request_roi))
 
@@ -89,8 +91,8 @@ class PredictSynapticPairs(BatchFilter):
 
         # pdb.set_trace()
 
-        del request[self.pred_pre_syn_points]
-        del request[self.pred_post_syn_points]
+        del request[self.pred_presyn_points_name]
+        del request[self.pred_postsyn_points_name]
 
 
 
@@ -118,35 +120,52 @@ class PredictSynapticPairs(BatchFilter):
 
 
         # for each affinity map
+        potential_pairs = {}
+
+        # For each affinity vector
         for aff_vec_i in range(pred_vol.shape[0]):
-            aff_vec = self.affinity_vectors[aff_vec_i]
-            aff_vec_vol = pred_vol[aff_vec_i,:,:,:]
-            aff_vec_vol = (aff_vec_vol > 0.5)*1.0
+            aff_vec = self.affinity_vectors[aff_vec_i]/voxel_size
+            aff_vec_vol_prob = pred_vol[aff_vec_i,:,:,:]
+            aff_vec_vol = (aff_vec_vol_prob > 0.5)*1.0
 
             logger.debug('Vector Hits: {}'.format(np.sum(aff_vec_vol)))
 
+            # Get all predicted blobs
             labels, label_count = ndimage.label(aff_vec_vol, str_3D)
             unique_labels, counts = np.unique(labels, return_counts=True)
 
+            # If there are blobs (apart from the background)
             if len(unique_labels) > 1:
                 unique_labels = unique_labels[1:]
                 counts = counts[1:]
+
+                # Get those with area larger than threhsold
                 selected_labels = (unique_labels[counts >= self.threshold]).tolist()
 
+                # If there any sufficiently large blobs
                 if len(selected_labels) > 0:
-                    pred_pre_syn_loc = np.asarray(ndimage.measurements.center_of_mass(aff_vec_vol, labels, selected_labels))
+
+                    # Get center of mass all blobs as potential pre_synaptic sites
+                    pred_pre_syn_loc = np.asarray(ndimage.measurements.center_of_mass(aff_vec_vol_prob,
+                        labels, selected_labels))
+
+                    cummulative_probabilites = np.asarray(ndimage.measurements.sum(aff_vec_vol_prob,
+                        labels, selected_labels))
+
+                    # Predict Post synaptic blobs by shifting by affinity vector
                     pred_post_syn_loc = pred_pre_syn_loc - aff_vec
 
-                    potential_pairs = {}
+                    # For each potential synaptic pair
+                    for pred_pre_loc, pred_post_loc, cum_prob in zip(pred_pre_syn_loc,
+                        pred_post_syn_loc, cummulative_probabilites):
 
-                    for pred_pre_loc, pred_post_loc in zip(pred_pre_syn_loc, pred_post_syn_loc):
-
+                        # Check in in bounds
                         index = tuple((np.round(pred_pre_syn_loc[0])).astype('int'))
 
                         if np.all(np.array([0,0,0]) <= index) and np.all(index <= seg_vol.shape):
                             pre_neuron_id = seg_vol[index]
                         else:
-                            logger.debug('Pre out of bounds: %s'%(index))
+                            logger.debug('Pre out of bounds: {}'.format(index))
                             continue
 
                         index = tuple((np.round(pred_post_syn_loc[0])).astype('int'))
@@ -154,22 +173,107 @@ class PredictSynapticPairs(BatchFilter):
                         if np.all(np.array([0,0,0]) <= index) and np.all(index <= seg_vol.shape):
                             post_neuron_id = seg_vol[index]
                         else:
-                            logger.debug('Post out of bounds: %s'%(index))
+                            logger.debug('Post out of bounds: {}'.format(index))
                             continue
 
-                        key = str(pre_neuron_id - post_neuron_id)
-                        if key != '0':
-                            if key in potential_pairs.keys():
-                                potential_pairs[key]['pre'].append(pred_pre_loc)
-                                potential_pairs[key]['post'].append(pred_post_loc)
-                            else:
-                                potential_pairs[key] = {'pre': [pred_pre_loc], 'post': [pred_post_loc]}
-                        else:
+                        # If connecting same neuron, ignore
+                        if pre_neuron_id == post_neuron_id:
                             logger.debug('Pre and Post match to same neuron: %s'%(post_neuron_id))
                             continue
 
+                        # with key pre_neuron_id-post_neuron_id
+                        key = '{}-{}'.format(pre_neuron_id, post_neuron_id)
 
-                    pdb.set_trace()
+                        if key in potential_pairs.keys():
+                            # TODO remove this test after sure it's fine
+                            if not (pre_neuron_id == potential_pairs[key]['pre']['neuron_id'] and \
+                                post_neuron_id == potential_pairs[key]['post']['neuron_id']):
+                                logger.debug('CRAZY: key:{} pre:{} post:{}'.format(key, pre_neuron_id, post_neuron_id))
+                                pdb.set_trace()
+                            else:
+                                potential_pairs[key]['pre']['loc'].append(pred_pre_loc)
+                                potential_pairs[key]['pre']['cum_prob'].append(cum_prob)
+
+                                potential_pairs[key]['post']['loc'].append(pred_post_loc)
+                                potential_pairs[key]['post']['cum_prob'].append(cum_prob)
+
+                        else:
+                            potential_pairs[key] = {
+                            'pre': {
+                                'loc':[pred_pre_loc],
+                                'cum_prob':[cum_prob],
+                                'neuron_id':pre_neuron_id
+                                },
+                            'post': {
+                                'loc':[pred_post_loc],
+                                'cum_prob':[cum_prob],
+                                'neuron_id':post_neuron_id
+                                },
+                            }
+
+        pred_presyn_data = {}
+        pred_postsyn_data = {}
+
+        local_synapse_id = 0
+        local_location_id = 0
+
+        pre_offset = request[self.pred_presyn_points_name].roi.get_offset()
+        post_offset = request[self.pred_postsyn_points_name].roi.get_offset()
+
+
+        for synapse_id, locations in potential_pairs.items():
+
+            # For all pre synaptic locations connecting the same two neurons average their locations
+            pre_loc = np.mean(locations['pre']['loc'],axis=0)
+            pre_neuron_id = seg_vol[tuple(np.round(pre_loc).astype('int'))]
+
+            # Check the average location is in the same neuron as the individual locations
+            if pre_neuron_id != locations['pre']['neuron_id']:
+                logger.warning('Pre average locations lies in different neuron than individuals\
+                 individual neuron_ids:{} average neuron_ids:{}'.format(
+                    locations['pre']['neuron_id'], pre_neuron_id))
+                pdb.set_trace()
+
+            # For all post synaptic locations connecting the same two neurons average their locations
+            post_loc = np.mean(locations['post']['loc'],axis=0)
+            post_neuron_id = seg_vol[tuple(np.round(post_loc).astype('int'))]
+
+            # Check the average location is in the same neuron as the individual locations
+            if post_neuron_id != locations['post']['neuron_id']:
+                logger.warning('Post average locations lies in different neuron than individuals\
+                 individual neuron_ids:{} average neuron_ids:{}'.format(
+                    locations['pre']['neuron_id'], post_neuron_id))
+                pdb.set_trace()
+
+            # Calculare local ids
+            local_synapse_id += 1
+            synapse_id = local_synapse_id
+
+            local_location_id += 1
+            pre_loc_id = local_location_id
+
+            local_location_id += 1
+            post_loc_id = local_location_id
+
+            # Create points
+            pred_presyn_data[pre_loc_id] = PreSynPoint(location=pre_loc + pre_offset,
+                location_id=pre_loc_id, synapse_id=synapse_id, partner_ids=[post_loc_id])
+
+            pred_postsyn_data[post_loc_id] = PostSynPoint(location=post_loc + post_offset,
+                location_id=post_loc_id, synapse_id=synapse_id, partner_ids=[pre_loc_id])
+
+
+        pre_spec = PointsSpec(roi=request[self.pred_presyn_points_name].roi)
+        batch.points[self.pred_presyn_points_name] = Points(pred_presyn_data, pre_spec)
+
+        post_spec = PointsSpec(roi=request[self.pred_postsyn_points_name].roi)
+        batch.points[self.pred_postsyn_points_name] = Points(pred_postsyn_data, post_spec)
+
+        # pdb.set_trace()
+
+        # Crop all other requests
+        for volume_type, volume in request.volume_specs.items():
+            batch.volumes[volume_type] = batch.volumes[volume_type].crop(volume.roi)
 
                 # ndimage.measurements.center_of_mass(b, lbl, [1,2])
 
